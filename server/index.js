17de9d3b6744b56d6050221400786f9aa43a6f85
Juanco <juanffo874@gmail.com>
2024-05-26 12:37:26 -0600

    Configuracion del servido y el puerto

--- server/index.js
+++ server/index.js
@@ -1,1 +1,8 @@
-console.log("Running");
+import express from 'express';
+
+const app = express();
+
+const port = 3000;
+
+app.listen(port);
+console.log(`El servidor esta funcionando en el puerto: ${port}`)
