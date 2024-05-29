67d3d4d40e265b47215849821edf72ab801b7655
Juanco <juanffo874@gmail.com>
2024-05-28 23:45:14 -0600

    Termino de los endpoints de obtener una tarea, y obtener todas la tareas, y sus consultas

--- server/controllers/task.controller.js
+++ server/controllers/task.controller.js
@@ -1,12 +1,24 @@
 import { pool } from "../db.js"
 
-const getTasks = (req, res) => {
-    res.send("Obteniendo tareas")
+const getTasks = async (req, res) => {
+    //Consulta
+    const [result] = await pool.query("SELECT * FROM tasks ORDER BY createdAt ASC")
+    //Mostrando el resultado
+    res.json(result)
 }
 
-const getTask = (req, res) => {
-    res.send("Obteniendo una tarea")
-}
+const getTask = async (req, res) => {
+    //Consulta
+    const [result] = await pool.query("SELECT * FROM tasks WHERE id = ?", [
+        req.params.id,
+    ]);
+
+    //Si no existe el resultado
+    result.length == 0 ? res.status(404).json({ message: "Tarea no encontrada"}) : null
+
+    //Mostrando el resultado
+    res.json(result[0]);
+};
 
 const createTask = async (req, res) => {
     //Extraer los datos del body
