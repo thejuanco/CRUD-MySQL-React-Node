624d0a27e86c0056b569ca13c4656045e31fbb60
Juanco <juanffo874@gmail.com>
2024-05-29 14:05:16 -0600

    Creacion de las primeras paginas de la app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,19 @@
+import { Route, Routes } from "react-router-dom"
+
+//Importando las vistas
+import TasksPage from "./pages/TasksPage"
+import TaskForm from "./pages/TaskForm"
+import NotFound from "./pages/NotFound"
 
 function App() {
 
   return (
     <>
-      <div>Hola mundo</div>
+      <Routes>
+        <Route path="/" element={<TasksPage />} />
+        <Route path="/new" element={<TaskForm />} />
+        <Route path="*" element={<NotFound/>}/>
+      </Routes>
     </>
   )
 }
---
+++ client/src/pages/TasksPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TasksPage = () => {
+  return (
+    <div>TaskPage</div>
+  )
+}
+
+export default TasksPage
