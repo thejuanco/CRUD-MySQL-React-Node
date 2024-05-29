cb3d20eef4a68d389de0c34c55e309171a70a229
Juanco <juanffo874@gmail.com>
2024-05-29 13:31:44 -0600

    Manejo de errores para evitar que se detenga el servidor

--- server/controllers/task.controller.js
+++ server/controllers/task.controller.js
@@ -1,67 +1,96 @@
 import { pool } from "../db.js"
 
 const getTasks = async (req, res) => {
-    //Consulta
-    const [result] = await pool.query("SELECT * FROM tasks ORDER BY createdAt ASC")
-    //Mostrando el resultado
-    res.json(result)
+    try {
+      //Consulta
+      const [result] = await pool.query(
+        "SELECT * FROM tasks ORDER BY createdAt ASC"
+      );
+      //Mostrando el resultado
+      res.json(result);
+    } catch (error) {
+      //Retorna el status y el mensaje del error
+      return res.status(500).json({ message: error.message });
+    }
 }
 
 const getTask = async (req, res) => {
-    //Consulta
-    const [result] = await pool.query("SELECT * FROM tasks WHERE id = ?", [
-        req.params.id,
-    ]);
+    try {
+        //Consulta
+        const [result] = await pool.query("SELECT * FROM tasks WHERE id = ?", [
+            req.params.id,
+        ]);
 
-    //Si no existe el resultado
-    result.length === 0 ? res.status(404).json({ message: "Tarea no encontrada"}) : null
+        //Si no existe el resultado
+        result.length === 0 ? res.status(404).json({ message: "Tarea no encontrada"}) : null
 
-    //Mostrando el resultado
-    res.json(result[0]);
+        //Mostrando el resultado
+        res.json(result[0]);
+
+    } catch (error) {
+        //Retorna el status y el mensaje del error
+        return res.status(500).json({ message: error.message });
+    }
 };
 
 const createTask = async (req, res) => {
-    //Extraer los datos del body
-    const { title, description } = req.body;
-    //Extrayendo el resultado de la query
-    const [result] = await pool.query(
-        // Query para insertar datos a la tabla
-      "INSERT INTO tasks (title, description) VALUES (?, ?)",
-      [
-        title,
-        description
-      ]
-    );
-    
-    //Mostrarndo el resultado
-    res.json({
-        id: result.insertId,
-        title,
-        description
-    })
+    try {
+        //Extraer los datos del body
+        const { title, description } = req.body;
+        //Extrayendo el resultado de la query
+        const [result] = await pool.query(
+            // Query para insertar datos a la tabla
+        "INSERT INTO tasks (title, description) VALUES (?, ?)",
+        [
+            title,
+            description
+        ]
+        );
+        
+        //Mostrarndo el resultado
+        res.json({
+            id: result.insertId,
+            title,
+            description
+        })
+
+    } catch (error) {
+        //Retorna el status y el mensaje del error
+        return res.status(500).json({ message: error.message });
+    }
 }
 
 const updateTask = async (req, res) => {
 
-   //Extraer los datos del body y consulta
-   const result = await pool.query("UPDATE tasks SET ? WHERE id = ?", [
-     req.body,
-     req.params.id,
-   ]);
+    try {
+        //Extraer los datos del body y consulta
+        const result = await pool.query("UPDATE tasks SET ? WHERE id = ?", [
+            req.body,
+            req.params.id,
+        ]);
 
-   res.json(result);
+        res.json(result);
 
+    } catch (error) {
+        //Retorna el status y el mensaje del error
+        return res.status(500).json({ message: error.message });
+    }
 }
 
 const deleteTask = async (req, res) => {
-    //Consulta
-    const [ result ] = await pool.query('DELETE FROM tasks WHERE id = ?', [req.params.id])
+    try {
+        //Consulta
+        const [ result ] = await pool.query('DELETE FROM tasks WHERE id = ?', [req.params.id])
 
-    //Validacion si no existe el resultado
-    result.affectedRows === 0 ?  res.status(404).json({ message: "Tarea no encontrada"}) : null
+        //Validacion si no existe el resultado
+        result.affectedRows === 0 ?  res.status(404).json({ message: "Tarea no encontrada"}) : null
 
-    //Retornando el status
-    return res.sendStatus(204)
+        //Retornando el status
+        return res.sendStatus(204)
+    } catch (error) {
+        //Retorna el status y el mensaje del error
+        return res.status(500).json({ message: error.message });
+    }
 }
 
 export {
