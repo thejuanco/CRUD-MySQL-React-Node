70176252cba0dc6c55e4d0fc1c1d7e776d107028
Juanco <juanffo874@gmail.com>
2024-05-29 23:37:00 -0600

    Instalacion de cors para permitir que todos los origenes se puedan conectar a la app

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 import express from 'express';
+import cors from 'cors';
 import { PORT } from './config.js';
 import indexRoutes from './routes/index.routes.js';
 import taskRoutes from './routes/task.routes.js';
 
 const app = express();
+
+//Habilitar cors para permitir que se conecte el servidor del client
+app.use(cors());
 
 //Procesar los datos del cliente
 app.use(express.json());
