8374080d71b657c36d4f5eb8468e8d4e2de26fbb
Juanco <juanffo874@gmail.com>
2024-05-29 23:17:50 -0600

    Primeros para la creacion del formulario con formik, y su instalacion

--- client/src/pages/TaskForm.jsx
+++ client/src/pages/TaskForm.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
+import { Formik, Form } from "formik";
 
 const TaskForm = () => {
   return (
-    <div>TaskForm</div>
-  )
-}
+    <div>
+      <Formik
+        //En lugar de crear un componente personalizado utlizamos formik para el formulario
+        initialValues={{
+          title: "",
+          description: "",
+        }}
+        onSubmit={(values) => {
+          console.log(values)
+        }}
+      >
+        {({ handleChange, handleSubmit }) => (
+          //Utilizando la propiedad handleChange para ver que tipea el usario en el formulario
+          <Form onSubmit={handleSubmit}>
+            <label>Title</label>
+            <input
+              type="text"
+              name="title"
+              placeholder="Write a title"
+              //Llenando el evento onChange con los datos que introduce el usuario
+              onChange={handleChange}
+            />
 
-export default TaskForm
+            <label>Description</label>
+            <textarea
+              name="description"
+              rows="3"
+              placeholder="Write a description"
+              onChange={handleChange}
+            ></textarea>
+
+            <button type="submit">Save</button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+};
+
+export default TaskForm;
