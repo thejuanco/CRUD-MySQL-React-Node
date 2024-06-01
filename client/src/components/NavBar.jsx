b830fb28a1b97866cb7e02fd896e1bc426891484
Juanco <juanffo874@gmail.com>
2024-06-01 10:35:29 -0600

    Dando estilos a la navegacion

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,22 +1,27 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <div>
-        <h1 className='m-4 font-semibold text-center'>React MySQL</h1>
+    <div className="bg-gray-100 flex justify-between px-10 py-4 mb-8">
+      <Link to="/">
+        <h1 className="font-bold text-left text-xl">Task Manager</h1>
+      </Link>
 
-        <ul className='space-y-4'>
-            <li>
-                <Link to="/" className='p-2 bg-slate-200 mx-2'>Home</Link>
-            </li>
-            <li>
-                <Link to="/new" className='p-2 bg-slate-200'>Create Task</Link>
-            </li>
-        </ul>
-
+      <ul className="flex">
+        <li>
+          <Link to="/" className="p-2 mx-2 hover:underline">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/new" className="p-2  rounded-lg hover:underline">
+            Create Task
+          </Link>
+        </li>
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
